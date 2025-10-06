2d466ee2309972b579ad5d4a201dbedb5861d882
Ruru <renehinrichsen@yahoo.dk>
2025-10-06 14:17:44 +0200

    Testing UserControls

---
+++ PetCareWPF/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareWPF.ViewModels
+{
+    public class HomeViewModel
+    {
+
+    }
+}
--- PetCareWPF/ViewModels/MainViewModel.cs
+++ PetCareWPF/ViewModels/MainViewModel.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace PetCareWPF.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // ViewModels for each section
+        public HomeViewModel HomeVM { get; set; }
+        public PetsViewModel PetsVM { get; set; }
+        public CustomersViewModel CustomersVM { get; set; }
+
+        // Commands for switching views
+        public ICommand HomeViewCommand { get; }
+        public ICommand PetsViewCommand { get; }
+        public ICommand CustomersViewCommand { get; }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            PetsVM = new PetsViewModel();
+            CustomersVM = new CustomersViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(_ => CurrentView = HomeVM);
+            PetsViewCommand = new RelayCommand(_ => CurrentView = PetsVM);
+            CustomersViewCommand = new RelayCommand(_ => CurrentView = CustomersVM);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
